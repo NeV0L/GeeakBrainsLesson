26248c6cd35e25386db65ccd353f57196934c56f
VolskiyNE <VolskiyNE@infteh.ru>
2018-02-25 22:43:02 +0300

    J3L4 Добавлена работа с файлом

--- J3Lesson4/src/ThreadPartOneHomeWork.java
+++ J3Lesson4/src/ThreadPartOneHomeWork.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Write a description of class J3Lesson4 here.
  *
@@ -12,17 +17,41 @@ public class ThreadPartOneHomeWork {
     private volatile char currentLetter = 'A';
     // количество повторений
     private final int counter = 5;
+    // Записи для файла
+    private ArrayList<String> arraysNote = new ArrayList<>();
+    private final String noteOne = "Something has to be written here for the first record...";
+    private final String noteTwo = "Something has to be written here for the second record...";
+    private final String noteThree = "Something has to be written here for the third record...";
+    private final String noteFour = "Something has to be written here for the fourth record...";
+    private final String noteFive = "Something has to be written here for the fifth record...";
+    private final String noteSix = "Something has to be written here for the sixth record...";
+    private final String noteSeven = "Something has to be written here for the seventh record...";
+    private final String noteEight = "Something has to be written here for the eighth record...";
+    private final String noteNine = "Something has to be written here for the ninth record...";
+    private final String noteTen = "Something has to be written here for the tenth record...";
+    // Период записи в файл, 20 мс
+    private final int duration = 20;
+    // файл
+    private final File fileThread = new File("fileThread.txt");
 
     public static void main(String[] args) {
         ThreadPartOneHomeWork hw = new ThreadPartOneHomeWork();
+        hw.first();
+        hw.second();
+    }
+
+    /**
+     * Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз, порядок должен быть именно ABСABСABС. Используйте wait/notify/notifyAll.
+     */
+    void first() {
         Thread tA = new Thread(() -> {
-            hw.printA();
+            printA();
         });
         Thread tB = new Thread(() -> {
-            hw.printB();
+            printB();
         });
         Thread tC = new Thread(() -> {
-            hw.printC();
+            printC();
         });
         tA.start();
         tB.start();
@@ -75,5 +104,66 @@ public class ThreadPartOneHomeWork {
                 ex.printStackTrace();
             }
         }
+    }
+
+    /**
+     * Написать совсем небольшой метод, в котором 3 потока построчно пишут данные в файл (штук по 10 записей, с периодом в 20 мс)
+     */
+    void second() {
+        appendArray();
+        clearFile();
+
+        Thread tA = new Thread(() -> {
+            printFile("A");
+        });
+        Thread tB = new Thread(() -> {
+            printFile("B");
+        });
+        Thread tC = new Thread(() -> {
+            printFile("C");
+        });
+        tA.start();
+        tB.start();
+        tC.start();
+    }
+
+    /**
+     * Очищаем файл перед заполнением
+     */
+    private void clearFile() {
+        try (FileWriter file = new FileWriter(fileThread)) {
+            file.write("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void printFile(String nameThread) {
+        synchronized (monitor) {
+            try (FileWriter file = new FileWriter(fileThread, true)) {
+                for (String str: arraysNote) {
+                    file.write(str + " by thread " + nameThread + ".\n");
+                    monitor.wait(duration);
+                }
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * добавляем записи для вывода в файл
+     */
+    private void appendArray() {
+        arraysNote.add(noteOne);
+        arraysNote.add(noteTwo);
+        arraysNote.add(noteThree);
+        arraysNote.add(noteFour);
+        arraysNote.add(noteFive);
+        arraysNote.add(noteSix);
+        arraysNote.add(noteSeven);
+        arraysNote.add(noteEight);
+        arraysNote.add(noteNine);
+        arraysNote.add(noteTen);
     }
 }
